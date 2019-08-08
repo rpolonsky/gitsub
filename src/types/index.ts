ee4da18466bdde45cc4bea0095ce10aa7d462127
Roman V Polonsky <rvpolonskiy@gmail.com>
2019-08-08 22:12:08 +0300

    Add typings; Add localforage

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export type UserInfo = {
+  id: number;
+  login: string;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+};
+
+export type FollowersSnapshot = {
+  date: Date;
+  followers: UserInfo[];
+};
+
+export type FollowersDiff = {
+  lost: UserInfo[];
+  gained: UserInfo[];
+  kept: UserInfo[];
+};
