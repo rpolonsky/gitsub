9719e70e854fc4e9ff48bb49dcb667ba1d75fdb0
Roman V Polonsky <rvpolonskiy@gmail.com>
2019-10-06 15:25:36 +0300

    Update footer

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import s from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={s.footer}>
-      <p>Copyright (c) 2019-{(new Date()).getFullYear()} Roman Polonsky. All rights reserved.</p>
+      <p>Copyright (c) 2019-{new Date().getFullYear()} Roman Polonsky. All rights reserved.</p>
       <p>
         By continuing to use this site, you agree to our use of cookies, localStorage and other
         tracking technologies.
